9faf71ff007c66d84e0f739d842ef20a1815b95a
Opity69 <lucasob11@gmail.com>
2021-10-30 16:22:48 +0800

    Add Stubs

--- Include/Collison.hpp
+++ Include/Collison.hpp
@@ -1,5 +1,122 @@
 #pragma once
+
+#include  <glm3/glm/glm.hpp>
 class Collison
 {
 };
 
+
+
+typedef int Transform;
+struct Sphere
+{
+
+	glm::fvec3 pos = {};
+	float radius = 1;
+
+	bool  intersect(const Sphere& other)
+	{
+		
+	}
+	
+};
+
+struct AABB
+{
+	glm::fvec3 min ={};
+	glm::fvec3 max ={};
+
+
+	bool intersect(const AABB& other)
+	{
+		// TODO()  intersect aabb
+		
+	}
+	
+};
+
+
+
+class CollisonObject{
+
+private:
+	Transform transform_;
+
+
+	
+	// Cache
+
+	mutable  AABB aabbCache;
+	mutable  bool aabb_cachec_state =false;
+
+	mutable  AABB RadiusCache;
+	mutable  bool Radisu_cachec_state =false;
+
+protected:
+	void SetTransfrom(const Transform& t)
+	{
+		
+		
+	}
+
+	Transform getTransform()const
+	{
+
+		return  transform_;
+	}
+
+public:
+
+	virtual  AABB RecomputeAABB()
+	{
+		return  {};
+	}
+
+	virtual float  RecomputeRadius()
+	{
+		
+	}
+	
+	virtual AABB  GetAABB()
+	{
+		return  {};
+		
+	}
+
+	virtual Sphere ToSphere()
+	{
+		return  {};
+	}
+
+
+	virtual  glm::fvec3  SupportFunction(const glm::fvec3& dir) = 0;
+	
+
+	
+};
+
+
+class CollisonShpere : public  CollisonObject{
+	float Radius;
+
+
+};
+
+
+class CollisonBox : public  CollisonObject{
+
+	glm::fvec3 half_exents;
+
+
+};
+
+class Plane : public  CollisonObject
+{
+	glm::fvec3 points;
+
+	mutable  glm::fvec3 normal_cache;
+	mutable  bool normal_state = false;
+	
+
+	
+};
--- Include/VissualObject.hpp
+++ Include/VissualObject.hpp
@@ -1,5 +1,29 @@
 #pragma once
-class VissualObject
+#include <glm3/glm/glm.hpp>
+
+typedef  int Transform;
+class IVissualObject
 {
+
+	glm::fvec4 color;
+
+	virtual void Draw() =0;
+	virtual Transform get_transform_();
+};
+
+
+
+class Shpere : public  IVissualObject
+{
+
+
+	
+};
+
+class Box : public  IVissualObject
+{
+
+
+	
 };
 
